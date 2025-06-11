c9ff6c1ea097988c2b74ed7aff84a2b333b2fba2
rohit220604 <rjrohit2264@gmail.com>
2025-06-12 00:12:45 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600 underline">
+      Hello Tailwind v4!
+    </h1>
+  )
+}
+
+export default App;
